f7c871c535e9802551d613d5af9deee11dab31a3
simon.marmorat <simon.marmorat>
2016-09-16 13:34:22 +0200

    first commit

---
+++ client/reducers/posts.js
@@ -0,0 +1,20 @@
+// a reducer takes in two things:
+// 1. the action
+// 2. copy of current state
+
+function posts(state = [], action) {
+    switch(action.type) {
+        case 'INCREMENT_LIKES':
+            const i = action.index;
+            console.log("Incrementing Likes");
+            return [
+                ...state.slice(0,i),
+                {...state[i], likes: state[i].likes+1}, 
+                ...state.slice(i+1)
+            ]
+        default:
+            return state;
+    }
+}
+
+export default posts;
